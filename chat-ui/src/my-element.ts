0222425672da481db2129849e3307fc03d7259f1
Simen Kristoffersen <136330755+simenkristoffers1@users.noreply.github.com>
2025-07-29 17:49:18 +0200

    fix: update name
    Release-As: 2.0.0

--- chat-ui/src/my-element.ts
+++ chat-ui/src/my-element.ts
@@ -15,7 +15,7 @@ export class MyElement extends LitElement {
    * Copy for the read the docs hint.
    */
   @property()
-  docsHint = 'Click on the Vite and Lit logos to leasrn moressddsssss'
+  docsHint = 'Click on the Vite and Lit logos to leasrn moressddssssss'
 
   /**
    * The number of times the button has been clicked.
