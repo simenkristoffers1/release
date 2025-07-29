6ae31948230b33089cd89318911c6b925ae0586b
Simen Kristoffersen <simen.kristoffersen2@visma.com>
2025-07-29 21:28:01 +0200

    feat: some new feat

--- chat-ui/src/my-element.ts
+++ chat-ui/src/my-element.ts
@@ -15,7 +15,7 @@ export class MyElement extends LitElement {
    * Copy for the read the docs hint.
    */
   @property()
-  docsHint = "Click on the Vite and Lit logos to leasrn moresssddssssss";
+  docsHint = "Click on the Vite and Lit logos to leasrn moresssddsssssss";
 
   /**
    * The number of times the button has been clicked.
